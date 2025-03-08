9931ddb218028f98632099e5be85e8cb1fff443c
Madhav Paudel <madhav.paudel40400@gmail.com>
2025-03-08 16:42:30 +0545

    adding thing i learn today

---
+++ CHAPTER 3 - STRINGS/Practice Problem/1_into_string.py
@@ -0,0 +1,27 @@
+
+# We can primarily write a string in these three ways.
+# a ='Madhav' # Single quoted string
+# print(a)
+# b = "Madhav"# Double quoted string
+# print(b)
+# c ='''Madhav'''# Triple quoted string
+# print(c)
+
+# here is string slicing 
+name="Madhav paudel"
+# if i want only madhav then 
+first_name=name[0:6] #here[starting=0;end=6]
+
+print(first_name)
+# again if i want only the last name then 
+last_name=name[7:13]
+
+print(last_name)
+# above all example are of positive slicing now turn for positive slicing 
+
+print(name[-11:-5])
+print(name[5:11])
+
+# name1="harry"
+# print(name1[-4:-1])
+# print(name1[1:4])
